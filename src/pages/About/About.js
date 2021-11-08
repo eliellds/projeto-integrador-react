f788d2e4ffedded41f482dc6a345093dbec52011
Natalia Floridi <nataliafloridi@gmail.com>
2021-11-08 11:22:46 -0300

    adicionado componente H1 e H2

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,15 +1,16 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Title from "../../components/micro/Title/Title";
+import H1 from "../../components/micro/Title/H1";
+import H2 from "../../components/micro/Title/H2";
 
 function About(props) {
     return (
         <>
             <main className="container-fluid mb-4 py-2 pb-3">
 
-                <Title className="title" title={<h1>Sobre</h1>}></Title>
+                <H1 h1="Sobre"/>
 
-                <h2 className="pt-2" id="historia">História</h2>
+                <H2 className="pt-2" id="historia" h2="História"/>
 
                 <p>O antiquário Velho Luxo nasceu em 2021, com o intuito de facilitar o acesso a peças antigas àqueles que as amam.Surgindo assim um e-commerce, que abrange todo território nacional, e com foco na qualidade e atendimento ao cliente.</p>
 
@@ -19,10 +20,12 @@ function About(props) {
 
                 <p>Encontre no Antiquário Velho Luxo, peças únicas, às quais acrescentarão charme e elegância na sua decoração.</p>
 
-                <h2 className="pt-2" id="missao">Missão</h2>
+                <H2 className="pt-2" id="missao" h2="Missão"/>
+     
                 <p>Ter o maior acervo de peças no cenário  nacional para atender os apreciadores de artes, antiguidades e afins.</p>
 
-                <h2 className="pt-2" id="visao">Visão</h2>
+                <H2 className="pt-2" id="visão" h2="Visão"/>
+        
                 <p>Ser um antiquário de referência com reconhecimento nacional.</p>
 
             </main>

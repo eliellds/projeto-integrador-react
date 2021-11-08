2da0a141fcdf42d2e8dd808c3ddabed4b5371528
FlavioMilani <frmilanij@gmail.com>
2021-11-08 18:00:21 -0300

    ...

--- src/pages/Success/Success.js
+++ src/pages/Success/Success.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import './Success.css'
-<<<<<<< HEAD
 import SuccessPage from '../../components/macro/SuccessPage/SuccessPage'
-=======
 import CompletedPurchase from '../../components/macro/completedPurchase/CompletedPurchase'
->>>>>>> 9622ccea7cc439c443de91d19eb665ed01a10c6a
+
 
 function Success(props) {
 
     return (
         <>
-<<<<<<< HEAD
-            <SuccessPage numPedido={999999} valorTotal="R$ 9999,99" prazo="01/01/2022"/>
-=======
-            <CompletedPurchase/>
->>>>>>> 9622ccea7cc439c443de91d19eb665ed01a10c6a
+            <SuccessPage numPedido={999999} valorTotal="R$ 9999,99" prazo="01/01/2025"/>
         </>
     )
 }

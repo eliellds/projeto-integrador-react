4dec8ef856c4c816857a8f695903b040126fa8c4
RDJoseDeSouza <jose441232@gmail.com>
2021-11-08 14:35:39 -0300

    pegando imagens de produtos

---
+++ src/components/macro/listProducts/ListProducts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CardProduct from '../Cards/Products/CardProduct'
+
+function ListProduct(props){
+    products = props.products || []
+    function listarProdutos(){
+        return products.map(function(product){})
+    }
+    return<>
+    </>
+
+}
+export default ListProduct

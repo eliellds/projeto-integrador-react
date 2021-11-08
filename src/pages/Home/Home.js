a65797452d6befee8d03205f6446615f6b20ba83
RDJoseDeSouza <jose441232@gmail.com>
2021-11-08 12:21:28 -0300

    começando modificaçoes na home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,10 +4,246 @@ import Banners from '../../components/macro/Banners/Banner'
 
 function Home(props) {
 
-    return(
+    return (
         <>
-        <div>Home</div>
-        <Banners/>
+
+            <Banners />
+            <h2>Promoções</h2>
+
+            {/* <!-- Inicio lista cards produtos promocoes--> */}
+            <div className="container container-cards">
+
+                <ul className="row lista-cards promocoes tamanho mb-5">
+
+                    {/* <!-- Inicio card protudo --> */}
+                    <li className="col-11 col-sm-8 col-md-5 col-lg-2 mb-4">
+
+                        {/* <!-- Imagem card --> */}
+                        <div className="caixa-imagem">
+                            <a href="" className="imagem-produto card-1"></a>
+                        </div>
+
+                        {/* <!-- Corpo card --> */}
+                        <div className="corpo-card">
+                            <a href="" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+
+                                {/* <!-- Botao comprar --> */}
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+
+                            </div>
+
+                        </div>
+
+                    </li>
+                    {/* <!-- Fim card produto --> */}
+
+                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
+                        <div className="caixa-imagem">
+                            <a href="./produto.html" className="imagem-produto card-1"></a>
+                        </div>
+                        <div className="corpo-card">
+                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
+                        <div className="caixa-imagem">
+                            <a href="./produto.html" className="imagem-produto card-1"></a>
+                        </div>
+                        <div className="corpo-card">
+                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
+                        <div className="caixa-imagem">
+                            <a href="./produto.html" className="imagem-produto card-1"></a>
+                        </div>
+                        <div className="corpo-card">
+                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 d-md-none d-lg-block mb-4">
+                        <div className="caixa-imagem">
+                            <a href="./produto.html" className="imagem-produto card-1"></a>
+                        </div>
+                        <div className="corpo-card">
+                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+
+            </div>
+            {/* <!-- Fim lista cards produtos promocoes --> */}
+
+
+            {/* <!-- INÍCIO DESTAQUES --> */}
+
+            <h2 className="mt-3">Destaques</h2>
+
+            <section className="container-fluid">
+
+                <div className="row custom-section mb-5 justify-content-center">
+
+                    <div className="col-md-5 col-12 custom-section-item">
+
+                        <img src="./natalia/imgs/caixaRegistradora.png" alt="Caixa Registradora" />
+
+                    </div>
+
+                    <div className="col-md-5 col-12 custom-section-item">
+
+                        <h6>Caixa registradora <br /> (1932)</h6>
+                        <p>R$ 8520,00</p>
+                        <button> Comprar</button>
+
+                    </div>
+
+                    <div className="col-md-5 col-12 custom-section-item">
+
+                        <h6>Louças Portuguesas <br />(1889)</h6>
+                        <p>R$ 2520,00</p>
+                        <button>Comprar</button>
+
+                    </div>
+
+                    <div className="col-md-5 col-12 custom-section-item">
+
+                        <img src="./natalia/imgs/loucasPortuguesas.jpg" alt="Louças Portuguesas" />
+
+                    </div>
+
+                </div>
+
+            </section>
+
+            {/* <!-- FINAL  DESTAQUES --> */}
+
+            <h2>Novidades</h2>
+
+            {/* <!-- Inicio lista cards produtos novidades --> */}
+            <div className="container container-cards">
+
+                <ul className="row lista-cards novidades tamanho mb-5">
+
+                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
+                        <div className="caixa-imagem">
+                            <a href="./produto.html" className="imagem-produto card-1"></a>
+                        </div>
+                        <div className="corpo-card">
+                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
+                        <div className="caixa-imagem">
+                            <a href="./produto.html" className="imagem-produto card-1"></a>
+                        </div>
+                        <div className="corpo-card">
+                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
+                        <div className="caixa-imagem">
+                            <a href="./produto.html" className="imagem-produto card-1"></a>
+                        </div>
+                        <div className="corpo-card">
+                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
+                        <div className="caixa-imagem">
+                            <a href="./produto.html" className="imagem-produto card-1"></a>
+                        </div>
+                        <div className="corpo-card">
+                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 d-md-none d-lg-block mb-4">
+                        <div className="caixa-imagem">
+                            <a href="./produto.html" className="imagem-produto card-1"></a>
+                        </div>
+                        <div className="corpo-card">
+                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
+                            <div className="pagamento">
+                                <div className="preco">
+                                    <div className="preco-de">R$999,59</div>
+                                    <div className="preco-por">R$ 850,59</div>
+                                </div>
+                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+
+            </div>
         </>
     )
 }

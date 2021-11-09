aaa9efb99772e63de789a613827de2f6d3931d50
RDJoseDeSouza <jose441232@gmail.com>
2021-11-09 09:54:46 -0300

    Jose funcionando

--- src/components/macro/listProducts/ListProducts.js
+++ src/components/macro/listProducts/ListProducts.js
@@ -4,7 +4,11 @@ import CardProduct from '../Cards/Products/CardProduct'
 function ListProduct(props){
     products = props.products || []
     function listarProdutos(){
-        return products.map(function(product){})
+        return products.map(function(product){
+            <CardProduct>
+                <img src={"../../../assets/images/products/"+product.image}/>
+            </CardProduct> 
+        })
     }
     return<>
     </>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './Home.css'
 import Banners from '../../components/macro/Banners/Banner'
 import ListProduct from '../../components/macro/Cards/Products/CardProduct'
+import Imagem from '../../assets/images/products/BandeijaTurca.webp'
+import Loucas from "../../assets/images/products/loucasPortuguesas.jpg"
+import Caixa from "../../assets/images/products/caixaRegistradora.png"
 function Home(props) {
 
     return (
@@ -12,15 +15,19 @@ function Home(props) {
 
             {/* <!-- Inicio lista cards produtos promocoes--> */}
             <div className="container container-cards">
-                <div className="row justify-content-center"> 
+                <ul className="row justify-content-md-between justify-content-sm-center "> 
                 
-                <ListProduct/>
-                <ListProduct/>
-                <ListProduct/>
-                <ListProduct/>
-                <ListProduct/>
-                <ListProduct/>
-                </div>
+                <ListProduct>
+                <img src={Imagem}/>
+                </ListProduct>
+                <ListProduct><img src={Imagem}/></ListProduct>
+                <ListProduct><img src={Imagem}/></ListProduct>
+                <ListProduct><img src={Imagem}/></ListProduct>
+                <ListProduct><img src={Imagem}/></ListProduct>
+                {/* <ListProduct><img src={Imagem}/></ListProduct> */}
+
+      
+                </ul>
 
              
              
@@ -39,7 +46,7 @@ function Home(props) {
 
                     <div className="col-md-5 col-12 custom-section-item">
 
-                        <img src="./natalia/imgs/caixaRegistradora.png" alt="Caixa Registradora" />
+                        <img src={Caixa} alt="Caixa Registradora" />
 
                     </div>
 
@@ -61,7 +68,7 @@ function Home(props) {
 
                     <div className="col-md-5 col-12 custom-section-item">
 
-                        <img src="./natalia/imgs/loucasPortuguesas.jpg" alt="Louças Portuguesas" />
+                        <img src={Loucas}alt="Louças Portuguesas" />
 
                     </div>
 
@@ -76,88 +83,15 @@ function Home(props) {
             {/* <!-- Inicio lista cards produtos novidades --> */}
             <div className="container container-cards">
 
-                <ul className="row lista-cards novidades tamanho mb-5">
+                <ul className="row row justify-content-md-between justify-content-sm-center mb-5">
 
-                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
-                        <div className="caixa-imagem">
-                            <a href="./produto.html" className="imagem-produto card-1"></a>
-                        </div>
-                        <div className="corpo-card">
-                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
-                            <div className="pagamento">
-                                <div className="preco">
-                                    <div className="preco-de">R$999,59</div>
-                                    <div className="preco-por">R$ 850,59</div>
-                                </div>
-                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
-                        <div className="caixa-imagem">
-                            <a href="./produto.html" className="imagem-produto card-1"></a>
-                        </div>
-                        <div className="corpo-card">
-                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
-                            <div className="pagamento">
-                                <div className="preco">
-                                    <div className="preco-de">R$999,59</div>
-                                    <div className="preco-por">R$ 850,59</div>
-                                </div>
-                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
-                        <div className="caixa-imagem">
-                            <a href="./produto.html" className="imagem-produto card-1"></a>
-                        </div>
-                        <div className="corpo-card">
-                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
-                            <div className="pagamento">
-                                <div className="preco">
-                                    <div className="preco-de">R$999,59</div>
-                                    <div className="preco-por">R$ 850,59</div>
-                                </div>
-                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 mb-4">
-                        <div className="caixa-imagem">
-                            <a href="./produto.html" className="imagem-produto card-1"></a>
-                        </div>
-                        <div className="corpo-card">
-                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
-                            <div className="pagamento">
-                                <div className="preco">
-                                    <div className="preco-de">R$999,59</div>
-                                    <div className="preco-por">R$ 850,59</div>
-                                </div>
-                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li className="col-12 col-sm-8 col-md-5 col-lg-2 d-md-none d-lg-block mb-4">
-                        <div className="caixa-imagem">
-                            <a href="./produto.html" className="imagem-produto card-1"></a>
-                        </div>
-                        <div className="corpo-card">
-                            <a href="./produto.html" className="descricao">Jogo de chá<br />(1889)</a>
-                            <div className="pagamento">
-                                <div className="preco">
-                                    <div className="preco-de">R$999,59</div>
-                                    <div className="preco-por">R$ 850,59</div>
-                                </div>
-                                <a href="./produto.html" className="btn-comprar">COMPRAR</a>
-                            </div>
-                        </div>
-                    </li>
-
+                <ListProduct>
+                <img src={Imagem}/>
+                </ListProduct>
+                <ListProduct><img src={Imagem}/></ListProduct>
+                <ListProduct><img src={Imagem}/></ListProduct>
+                <ListProduct><img src={Imagem}/></ListProduct>
+                <ListProduct><img src={Imagem}/></ListProduct>
                 </ul>
 
             </div>

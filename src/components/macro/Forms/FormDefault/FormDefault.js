7a9b12ac822c75fbd7480171c4ad776173767913
FlavioMilani <frmilanij@gmail.com>
2021-11-10 16:27:41 -0300

    cart react

--- src/components/macro/Forms/FormDefault/FormDefault.js
+++ src/components/macro/Forms/FormDefault/FormDefault.js
@@ -7,12 +7,9 @@ function FormDefault(props) {
         <>
             <div className={props.class}>
                 <h1 className="custom-title">{props.title}</h1>
-<<<<<<< HEAD
+
                 <form className={props.formClass} action={props.action} method={props.method}>
-=======
-                <form classNameName="form-input" onClick={props.click}>
->>>>>>> 3163711a4fd162dc519ece239e72f2ade8421860
-                    {props.children}
+
                 </form>
             </div>
         </>

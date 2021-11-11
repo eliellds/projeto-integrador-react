cc4cb86a54b2670f58ac817e0743838db0dfa8dc
RDJoseDeSouza <jose441232@gmail.com>
2021-11-11 17:31:40 -0300

    Correção de history

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Checkout.css'
+import { useHistory } from 'react-router';
 import FormShippigAddress from '../../components/macro/Forms/FormCheckout/FormShippigAddress';
 import FormDataCard from '../../components/macro/Forms/FormCheckout/FormDataCard';
 // import FormDataBankSlip from '../../components/macro/Forms/FormCheckout/FormDataBankSlip';
 import Button from '../../components/micro/Button/Button';
 
 function Checkout(props) {
-
+    const history = useHistory()
     function envio() {
         for( var index = 0; index < document.forms.length; index++ ) {
             document.forms[index].submit();
@@ -23,7 +24,7 @@ function Checkout(props) {
                 <FormDataCard />
 
                 <div className="row justify-content-around py-4">
-                    <Button label="Voltar" navigation route="/cart" class="btn-retorno"  />
+                    <Button label="Voltar" onclick={history.goBack} class="btn-retorno"  />
                     <Button onclick={envio} label="Finalizar"  class="btn-confirmacao" type="submit" />
                 </div>
 

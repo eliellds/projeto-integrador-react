aacc1d4ab83c355c63ce3ec3425b41028a435417
Natalia Floridi <nataliafloridi@gmail.com>
2021-12-01 09:49:38 -0300

    adicionado css form shiping addres

--- src/components/macro/Forms/FormCheckout/FormShippigAddress.js
+++ src/components/macro/Forms/FormCheckout/FormShippigAddress.js
@@ -16,6 +16,8 @@ import InputHook from "../../../micro/Forms/Input/InputHook"
 import { Redirect } from "react-router-dom";
 import Loading from "../../../../assets/images/success/loading.gif"
 import InputCard from "../../../micro/Forms/Input/InputCard";
+import OrderInfo from "../../../micro/productsSucess/orderInfo"
+import "./FormShippingAddress.css"
 
 const initial = {
 
@@ -128,18 +130,18 @@ function FormShippigAddress(props) {
                     var number = res.data.telephone.number.toString()
 
                     const parte1 = number.charAt(0) + number.charAt(1)
-                    const parte2 = number.slice(2,7)
-                    const parte3 = number.slice(7,11)
+                    const parte2 = number.slice(2, 7)
+                    const parte3 = number.slice(7, 11)
 
                     const numeroAjustado = "(" + parte1 + ") " + parte2 + "-" + parte3
 
                     setValue('telefone', numeroAjustado, { shouldValidate: true })
                 } else {
                     var number = res.data.telephone.number.toString()
-                    
+
                     const parte1 = number.charAt(0) + number.charAt(1)
-                    const parte2 = number.slice(2,6)
-                    const parte3 = number.slice(6,10)
+                    const parte2 = number.slice(2, 6)
+                    const parte3 = number.slice(6, 10)
 
                     const numeroAjustado = "(" + parte1 + ") " + parte2 + "-" + parte3
 
@@ -167,12 +169,14 @@ function FormShippigAddress(props) {
             return alert("Preencha os dados corretamente")
         }
 
- 
+
         var tempOrder = { ...order }
 
-        tempOrder = ({ ...tempOrder, 
-            telephone: { ...tempOrder.telephone, number: tempOrder.telephone.number.toString().replace(/[^0-9]/g, "")}, 
-            card: { ...tempOrder.card, cardNumber: criptCard(tempOrder.card.cardNumber), dueDate: inputYear + "-" + inputMonth + "-01" } })
+        tempOrder = ({
+            ...tempOrder,
+            telephone: { ...tempOrder.telephone, number: tempOrder.telephone.number.toString().replace(/[^0-9]/g, "") },
+            card: { ...tempOrder.card, cardNumber: criptCard(tempOrder.card.cardNumber), dueDate: inputYear + "-" + inputMonth + "-01" }
+        })
 
         let orderJson = JSON.stringify(tempOrder)
 
@@ -205,12 +209,12 @@ function FormShippigAddress(props) {
                 case "VISA":
                     setOrder({ ...order, card: { ...order.card, flag: { id: 2 }, cardNumber: card } })
                     setInputBrand("VISA")
-                    return true 
+                    return true
                     break;
                 case "MASTERCARD":
                     setOrder({ ...order, card: { ...order.card, flag: { id: 1 }, cardNumber: card } })
                     setInputBrand("MASTERCARD");
-                    return true 
+                    return true
                     break;
                 case "AMEX":
                     setOrder({ ...order, card: { ...order.card, flag: { id: 3 }, cardNumber: card } })
@@ -223,22 +227,22 @@ function FormShippigAddress(props) {
                 case "ELO":
                     setOrder({ ...order, card: { ...order.card, flag: { id: 4 }, cardNumber: card } })
                     setInputBrand("ELO");
-                    return true 
+                    return true
                     break;
                 case "HIPERCARD":
                     setOrder({ ...order, card: { ...order.card, flag: { id: 5 }, cardNumber: card } })
                     setInputBrand("HIPERCARD");
-                    return true 
+                    return true
                     break;
                 case "DINERS":
                     setOrder({ ...order, card: { ...order.card, flag: { id: 6 }, cardNumber: card } })
                     setInputBrand("DINERS CLUB");
-                    return true 
+                    return true
                     break;
                 default:
                     return setInputBrand("Cartão não aceito!")
             }
-        } 
+        }
         return false
     }
 
@@ -251,7 +255,7 @@ function FormShippigAddress(props) {
 
         if (dateCurrent > order.card.birthDate) {
 
-            if(MoipValidator.isValidNumber(data.CardNum) == true) {
+            if (MoipValidator.isValidNumber(data.CardNum) == true) {
                 if (MoipValidator.isSecurityCodeValid(data.CardNum, data.cvv) == true) {
                     if (MoipValidator.isExpiryDateValid(inputMonth, inputYear) == true) {
                         postOrder()
@@ -259,7 +263,7 @@ function FormShippigAddress(props) {
                         window.alert("Preencha a validade do cartão corretamente")
                     }
                 } else {
-                   window.alert("Preencha o CVV corretamente")
+                    window.alert("Preencha o CVV corretamente")
                 }
             } else {
                 window.alert("Preencha o número do cartão corretamente")
@@ -409,9 +413,9 @@ function FormShippigAddress(props) {
         setCheck(true)
         setCpf(true)
         clearErrors(["cpf"]) // limpa o erro ao clicar no campo CPF quando este exibe erro
-        var cpf = e;          
+        var cpf = e;
         cpf = cpf.toString().replace(/[^0-9]/g, ""); // transforma os valores digitados para apenas numeros
-        setOrder({...order, card:{...order.card, cpf: cpf }})
+        setOrder({ ...order, card: { ...order.card, cpf: cpf } })
         let booleano = true
         if (cpf == "") {
             // setCheck(false)
@@ -503,27 +507,27 @@ function FormShippigAddress(props) {
 
         setValue("CardNum", cartao)
         var validacoes = [
-        {  flag: 'Amex', regex: /^3[47][0-9]{13, 14}$/}
-        ,{ flag: 'Aura', regex: /^((?!504175))^((?!5067))(^50[0-9])/}
-        ,{ flag: 'BaneseCard', regex: /'^636117'/}
-        ,{ flag: 'Cabal', regex: /'(60420[1-9]|6042[1-9][0-9]|6043[0-9]{2}|604400)'/}
-        ,{ flag: 'Diners', regex: /'(36[0-8][0-9]{3}|369[0-8][0-9]{2}|3699[0-8][0-9]|36999[0-9])/}
-        ,{ flag: 'Discover', regex: /^6(?:011|5[0-9]{2})[0-9]{12}/}
-        ,{ flag: 'Elo', regex: /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/}
-        ,{ flag: 'FortBrasil', regex: /'^628167'/}
-        ,{ flag: 'GrandCard', regex: /'^605032'/}
-        ,{ flag: 'Hipercard', regex: /^ 606282 |^ 3841(?: [0 | 4 | 6]{ 1})0/}
-        ,{ flag: 'JCB', regex: /^(?:2131|1800|35\d{3})\d{11}/}
-        ,{ flag: 'Mastercard', regex: /^ ((5(([1 - 2] | [4 - 5])[0 - 9]{ 8} | 0((1 | 6)([0 - 9]{ 7})) | 3(0(4((0 | [2 - 9])[0 - 9]{ 5}) | ([0 - 3] | [5 - 9])[0 - 9]{ 6}) | [1 - 9][0 - 9]{ 7}))) | ((508116) \\d{ 4, 10 })| ((502121) \\d{ 4, 10 })| ((589916) \\d{ 4, 10 })| (2[0 - 9]{ 15 })| (67[0 - 9]{ 14 })| (506387) \\d{ 4, 10 })/}
-        ,{ flag: 'PersonalCard', regex: /'^636085'/}
-        ,{ flag: 'Sorocred', regex: /'^627892|^636414'/}
-        ,{ flag: 'Valecard', regex: /'^606444|^606458|^606482'/}
-        ,{ flag: 'Visa', regex: /^ 4[0 - 9]{ 15 } $/}
+            { flag: 'Amex', regex: /^3[47][0-9]{13, 14}$/ }
+            , { flag: 'Aura', regex: /^((?!504175))^((?!5067))(^50[0-9])/ }
+            , { flag: 'BaneseCard', regex: /'^636117'/ }
+            , { flag: 'Cabal', regex: /'(60420[1-9]|6042[1-9][0-9]|6043[0-9]{2}|604400)'/ }
+            , { flag: 'Diners', regex: /'(36[0-8][0-9]{3}|369[0-8][0-9]{2}|3699[0-8][0-9]|36999[0-9])/ }
+            , { flag: 'Discover', regex: /^6(?:011|5[0-9]{2})[0-9]{12}/ }
+            , { flag: 'Elo', regex: /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/ }
+            , { flag: 'FortBrasil', regex: /'^628167'/ }
+            , { flag: 'GrandCard', regex: /'^605032'/ }
+            , { flag: 'Hipercard', regex: /^ 606282 |^ 3841(?: [0 | 4 | 6]{ 1})0/ }
+            , { flag: 'JCB', regex: /^(?:2131|1800|35\d{3})\d{11}/ }
+            , { flag: 'Mastercard', regex: /^ ((5(([1 - 2] | [4 - 5])[0 - 9]{ 8} | 0((1 | 6)([0 - 9]{ 7})) | 3(0(4((0 | [2 - 9])[0 - 9]{ 5}) | ([0 - 3] | [5 - 9])[0 - 9]{ 6}) | [1 - 9][0 - 9]{ 7}))) | ((508116) \\d{ 4, 10 })| ((502121) \\d{ 4, 10 })| ((589916) \\d{ 4, 10 })| (2[0 - 9]{ 15 })| (67[0 - 9]{ 14 })| (506387) \\d{ 4, 10 })/ }
+            , { flag: 'PersonalCard', regex: /'^636085'/ }
+            , { flag: 'Sorocred', regex: /'^627892|^636414'/ }
+            , { flag: 'Valecard', regex: /'^606444|^606458|^606482'/ }
+            , { flag: 'Visa', regex: /^ 4[0 - 9]{ 15 } $/ }
         ]
 
         validacoes.map(item => {
             if (item.regex.test(cartao)) {
-                
+
                 setInputBrand(item.flag)
 
             }
@@ -535,8 +539,17 @@ function FormShippigAddress(props) {
     }
 
     return (
+
         <>
+
             <FormDefault id="address" title="Dados de entrega" action="/order">
+
+                <div class="container col-12 col-lg-5 mx-0 new-address">
+
+                    teste
+
+                </div>
+
 
                 <div class="row  justify-content-center mb-3">
 
@@ -741,7 +754,7 @@ function FormShippigAddress(props) {
                                 value={cvv}
                                 className="form-input col-12"
                                 placeholder="CVV" />
-                       
+
                         </div>
 
                         <div className=" col-6 col-md-2">
@@ -782,8 +795,8 @@ function FormShippigAddress(props) {
 
                                 {/* {MoipValidator.isExpiryDateValid(cardNumber, cvv) ? "" : <span className="text-danger">Insira uma data válida!</span>} */}
 
-                                <Input change={e => setInputMonth(e.target.value)} label="Mês" classCustom="col-6" type="text" placeholder="MM" maxlength={2}/>
-                                <Input change={e => setInputYear(e.target.value)} label="Ano" classCustom="col-6" type="text" placeholder="AA" maxlength={2}/>
+                                <Input change={e => setInputMonth(e.target.value)} label="Mês" classCustom="col-6" type="text" placeholder="MM" maxlength={2} />
+                                <Input change={e => setInputYear(e.target.value)} label="Ano" classCustom="col-6" type="text" placeholder="AA" maxlength={2} />
                             </div>
                         </div>
 

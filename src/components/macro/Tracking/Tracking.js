60d14d56094cf9078fd65265a5833e6ea20226a3
RDJoseDeSouza <jose441232@gmail.com>
2021-12-03 16:56:35 -0300

    Alterando traking e Pedidos

--- src/components/macro/Tracking/Tracking.js
+++ src/components/macro/Tracking/Tracking.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Payment from '../../../assets/images/tracking/pagar.png'
 import Separando from '../../../assets/images/tracking/packaging.png'
 import Rota from '../../../assets/images/tracking/enviado.png'
+import Recebido from '../../../assets/images/tracking/receber.png'
+
 import './Tracking.css'
 
 
@@ -21,25 +23,52 @@ function TrackingPack(props) {
                             <div className={"col-1 tracking-img-col " }>
                                 <img src={Payment} className={ props.status > 1? "completedTask images-tracking": " images-tracking" }/>
                             </div>
-                            <div className={props.status > 1?"col-2 line default-line align-self-center completedTask ": "col-3 line default-line align-self-center"}>
+                            <div className={props.status > 1?"col-2 line default-line align-self-center completedTask ": "col-2 line default-line align-self-center"}>
                             </div>
                             <div className="col-1 tracking-img-col">
-                                <img src={Separando}  className={ props.status > 2? "completedTask images-tracking": " images-tracking" }/>
+                        
+                                <img src={Separando}  className={ props.status > 2? "completedTask images-tracking col-12": " images-tracking col-12" }/>
+                     
+                           
                             </div>
-                            <div className={props.status > 2?"col-2 line default-line align-self-center completedTask ": "col-3 line default-line align-self-center"}>
+                            <div className={props.status > 2?"col-2 line default-line align-self-center completedTask ": "col-2 line default-line align-self-center"}>
                             </div>
                             <div className="col-1 tracking-img-col">
                                 <img src={Rota} className={ props.status > 3? "completedTask images-tracking": " images-tracking" } />
                             </div>
+                            <div className={props.status > 3?"col-2 line default-line align-self-center completedTask ": "col-2 line default-line align-self-center"}>
+                            </div>
+                            <div className="col-1 tracking-img-col">
+                                <img src={Recebido} className={ props.status == 4? "completedTask images-tracking": " images-tracking" } />
+                            </div>
+                        
 
-
-                           
+                          
                         </div>
                     </div>
 
-               
+                    <div className="col-12 my-2 pb-2">
+                        <div className="row justify-content-center text-center">
+                            <div className="col-2">Aguardando <br/> Pagamento</div>
+                            <div className="col-1 tracking-img-col">
+                               
+                            </div>
+                            <div className="col-2 ">Separando <br/> Pedido</div>
+                            <div className="col-1 tracking-img-col">
+                               
+                               </div>
+                            <div className="col-2">Em <br/> Transito</div>
+                            <div className="col-1 tracking-img-col">
+                               
+                               </div>
+                            <div className="col-2">Entregue</div>
 
-                
+
+                        </div>
+
+                    </div>
+
+                    <hr/>
 
             </div>
 
